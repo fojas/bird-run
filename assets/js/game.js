0262bb238d3283fce7d9292511054250fa60df67
Anthony U Fojas <anthony.fojas@vibes.com>
2015-08-02 20:48:52 -0500

    added characters and title screen

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,17 +3,32 @@ var game = new Phaser.Game(1300, 600, Phaser.AUTO, 'game');
 
 main = {
   preload: function () {
+    game.stage.backgroundColor = '#87CEEB';
     game.load.tilemap('map', 'assets/maps/level1.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('ground_1x1', 'assets/images/ground_1x1.png');
     game.load.image('phaser', 'assets/images/bird.png');
-    game.load.spritesheet('coin', 'assets/images/coin.png', 32, 32);
+    game.load.image('penguin', 'assets/images/penguin.png');
+    game.load.image('woodpecker', 'assets/images/woodpecker.png');
+    game.load.spritesheet('mouse', 'assets/images/mouse.png', 32, 32);
   },
 
+  findTilesByIndex: function(value, layer) {
+    var result = [];
+    layer.layer.data.forEach(function (v) {
+      v.forEach(function (tile) {
+        if(tile.index  === value) {
+          result.push(tile);
+        }
+      });
+    });
+
+    return result;
+  },
   create : function() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     this.map = game.add.tilemap('map');
     this.map.addTilesetImage('ground_1x1');
-    this.map.addTilesetImage('coin');
+    this.map.addTilesetImage('mouse');
     this.map.setCollisionBetween(1, 12);
 
     //  This will set Tile ID 26 (the coin) to call the hitCoin function when collided with
@@ -26,10 +41,11 @@ main = {
 
     this.layer = this.map.createLayer('Tile Layer 1');
 
+    this.coinCount = this.findTilesByIndex(26, this.layer).length
     this.layer.resizeWorld();
     this.sprites = []
-    this.addSprite(160,120,'phaser')
-    this.addSprite(260,120,'phaser')
+    this.addSprite(160,120,'penguin')
+    this.addSprite(1120,120,'woodpecker')
 
     this.cursors = [
       game.input.keyboard.createCursorKeys(),
@@ -51,7 +67,7 @@ main = {
   addSprite : function(x,y,name){
     var sprite = game.add.sprite(x,y, name);
     sprite.anchor.set(0.5);
-    sprite.scale.setTo(.5, .5)
+    sprite.scale.setTo(.75, .75)
     game.physics.enable(sprite);
 
     game.physics.arcade.gravity.y = 100;
@@ -64,7 +80,12 @@ main = {
   },
   hitCoin: function(sprite, tile) {
       tile.alpha = 0.2;
-      if(!tile.collected) this.addPoint(sprite.index);
+      if(!tile.collected) {
+        this.addPoint(sprite.index);
+        if(0 == --this.coinCount) {
+          game.state.start('title'); 
+        }
+      }
       tile.collected = true
       this.layer.dirty = true;
       return false;
@@ -79,9 +100,10 @@ main = {
       game.physics.arcade.collide(sprite, this.layer);
       sprite.body.velocity.x = 0;
       if (this.cursors[index].up.isDown) {
-        if (sprite.body.onFloor()) {
-          sprite.body.velocity.y = -200;
-        }
+        sprite.body.velocity.y = -200;
+      }
+      else if (this.cursors[index].down.isDown) {
+        sprite.body.velocity.y = 150;
       }
 
       if (this.cursors[index].left.isDown) {
@@ -96,8 +118,32 @@ main = {
   render: function () {
   }
 }
+
+var title = {
+  preload: function(){
+    game.stage.backgroundColor = '#FFA500';
+    game.load.image('title', 'assets/images/title.png');
+
+  },
+  create: function(){
+    var gameTitle = this.add.image(240,30,"title");
+    gameTitle.angle = (2+Math.random()*5)*(Math.random()>0.5?1:-1);
+    var titleTween = this.add.tween(gameTitle);
+    titleTween.to({
+      angle: -gameTitle.angle
+    },500+Math.random()*500,Phaser.Easing.Linear.None,true,0,1000,true);
+    this.game.add.text(480, 550, "Press Space to Start", { font: "30px Arial", fill: "#ffffff" }),
+    this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+      .onDown.add(function(){
+        game.state.start('main')
+      }, this); 
+
+  }, 
+  render : $.noop,update: $.noop
+}
 // Add and start the 'main' state to start the game
 game.state.add('main', main);  
-game.state.start('main'); 
+game.state.add('title', title);  
+game.state.start('title'); 
 
 }(Phaser, jQuery, _)
